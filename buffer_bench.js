c622570d7cbf677de10b66c6e7fdbe63a9b00155
Matt Ranney <mjr@ranney.com>
2010-09-17 01:41:35 -0700

    Add toString() comparison.

--- buffer_bench.js
+++ buffer_bench.js
@@ -1,8 +1,29 @@
 var source = new Buffer(100),
-    dest = new Buffer(100), i, j, k, count = 1000000, bytes = 100;
+    dest = new Buffer(100), i, j, k, tmp, count = 1000000, bytes = 100;
     
 for (i = 99 ; i >= 0 ; i--) {
     source[i] = 120;
+}
+
+for (i = bytes ; i > 0 ; i --) {
+    var start = new Date();
+    for (j = count ; j > 0; j--) {
+        tmp = source.toString("ascii", 0, bytes);
+    }
+    var end = new Date();
+    console.log("toString() " + i + " bytes " + (end - start) + " ms");
+}
+
+for (i = bytes ; i > 0 ; i --) {
+    var start = new Date();
+    for (j = count ; j > 0; j--) {
+        tmp = "";
+        for (k = 0; k <= i ; k++) {
+            tmp += String.fromCharCode(source[k]);
+        }
+    }
+    var end = new Date();
+    console.log("manual string " + i + " bytes " + (end - start) + " ms");
 }
 
 for (i = bytes ; i > 0 ; i--) {
