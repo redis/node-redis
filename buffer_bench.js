09465ff6be0256539ecbd30d4fe9ed6f40f412a8
Matt Ranney <mjr@ranney.com>
2010-09-17 01:17:19 -0700

    Test the speed of different ways of copying a small buffer.

---
+++ buffer_bench.js
@@ -0,0 +1,37 @@
+var source = new Buffer(100),
+    dest = new Buffer(100), i, j, k, count = 1000000, bytes = 100;
+    
+for (i = 99 ; i >= 0 ; i--) {
+    source[i] = 120;
+}
+
+for (i = bytes ; i > 0 ; i--) {
+    var start = new Date();
+    for (j = count ; j > 0 ; j--) {
+        for (k = i ; k > 0 ; k--) {
+            dest[k] = source[k];
+        }
+    }
+    var end = new Date();
+    console.log("Manual copy " + i + " bytes " + (end - start) + " ms");
+}
+
+for (i = bytes ; i > 0 ; i--) {
+    var start = new Date();
+    for (j = count ; j > 0 ; j--) {
+        for (k = i ; k > 0 ; k--) {
+            dest[k] = 120;
+        }
+    }
+    var end = new Date();
+    console.log("Direct assignment " + i + " bytes " + (end - start) + " ms");
+}
+
+for (i = bytes ; i > 0 ; i--) {
+    var start = new Date();
+    for (j = count ; j > 0 ; j--) {
+        source.copy(dest, 0, 0, i);
+    }
+    var end = new Date();
+    console.log("Buffer.copy() " + i + " bytes " + (end - start) + " ms");
+}
