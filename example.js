b48105eeb39f9626f476d3bae85de91894af2d22
Matt Ranney <mjr@ranney.com>
2010-09-17 14:23:33 -0700

    Update example to use new pre-connection queue.

--- example.js
+++ example.js
@@ -1,16 +1,13 @@
 var redis = require("redis"),
     client = redis.createClient();
 
-client.on("connect", function () {
-    client.set("string key", "string val", redis.print);
-    client.hset("hash key", "hashtest 1", "some value", redis.print);
-    client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-    client.hkeys("hash key", function (err, replies) {
-        console.log(replies.length + " replies:");
-        replies.forEach(function (reply, i) {
-            console.log("    " + i + ": " + reply);
-        });
-        client.end();
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
     });
+    client.end();
 });
-
