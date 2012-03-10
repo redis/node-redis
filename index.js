40fec1e419354420ffd72027121146d923e3a592
Lin Gui <guileen@gmail.com>
2012-03-11 03:11:37 +0800

    fix #186, multi/exec callback error

--- index.js
+++ index.js
@@ -954,7 +954,11 @@ Multi.prototype.exec = function (callback) {
                 if (typeof cur[cur.length - 1] === "function") {
                     cur[cur.length - 1](err);
                 } else {
-                    throw new Error(err);
+                    if (callback) {
+                    	callback(new Error(err));
+                    } else {
+                    	throw new Error(err);
+                    }
                 }
                 self.queue.splice(index, 1);
             }
