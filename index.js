f075f0fecab5628e330ceae53627d3a301fbdc25
lee <leed@ibcos.co.uk>
2013-12-23 14:57:04 +0000

    fixed typo\r\nadded check for null or unedfined to last argument in each command in…

--- index.js
+++ index.js
@@ -1072,10 +1072,12 @@ Multi.prototype.exec = function (callback) {
     // drain queue, callback will catch "QUEUED" or error
     // TODO - get rid of all of these anonymous functions which are elegant but slow
     this.queue.forEach(function (args, index) {
-        var command = args[0], obj;
+        var command = args[0],
+			finalArg = args[args.length - 1],
+			obj;
         
 		// if the last argument is null, undefined or a function remove it from processing.
-        if (typeof args[args.length - 1] "function" || /null|undefined/.test(args[args.length - 1])) {
+        if (typeof finalArg == "function" || /null|undefined/.test(finalArg)) {
             args = args.slice(1, -1);
         } else {
             args = args.slice(1);
