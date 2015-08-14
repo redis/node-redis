939ac61b8fea486c84084625f3ae0f9a59abed89
Blain Smith <rebelgeek@blainsmith.com>
2015-08-14 15:11:06 -0400

    no need for the source on this branch

--- examples/auth.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var redis  = require("redis"),
-    client = redis.createClient();
-
-// This command is magical.  Client stashes the password and will issue on every connect.
-client.auth("somepass");
--- test/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-node ./test/test.js false hiredis
-node ./test/test.js false javascript
