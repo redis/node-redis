359820c766f13716e83aebe898a530612ffd1d4c
Ruben Bridgewater <ruben.bridgewater@fintura.de>
2016-03-16 23:40:25 +0100

    Support redis 2.4 info command
    Fixes #1008

--- test/commands/info.spec.js
+++ test/commands/info.spec.js
@@ -12,14 +12,14 @@ describe("The 'info' method", function () {
         describe("using " + parser + " and " + ip, function () {
             var client;
 
-            before(function (done) {
+            beforeEach(function (done) {
                 client = redis.createClient.apply(null, args);
                 client.once("ready", function () {
                     client.flushall(done);
                 });
             });
 
-            after(function () {
+            afterEach(function () {
                 client.end(true);
             });
 
@@ -41,9 +41,10 @@ describe("The 'info' method", function () {
                 client.set('foo', 'bar');
                 client.info('keyspace');
                 client.select(2, function () {
-                    assert.strictEqual(Object.keys(client.server_info).length, 3, 'Key length should be three');
-                    assert(typeof client.server_info.db2 === 'object', 'db2 keyspace should be an object');
+                    assert.strictEqual(Object.keys(client.server_info).length, 2, 'Key length should be three');
+                    assert.strictEqual(typeof client.server_info.db0, 'object', 'db0 keyspace should be an object');
                 });
+                client.info(['keyspace']);
                 client.set('foo', 'bar');
                 client.info('all', function (err, res) {
                     assert(Object.keys(client.server_info).length > 3, 'Key length should be way above three');
@@ -51,6 +52,17 @@ describe("The 'info' method", function () {
                     assert.strictEqual(typeof client.server_info.db2, 'object');
                     done();
                 });
+            });
+
+            it('check redis v.2.4 support', function (done) {
+                var end = helper.callFuncAfter(done, 2);
+                client.send_command = function (command, args, callback) {
+                    assert.strictEqual(args.length, 0);
+                    assert.strictEqual(command, 'info');
+                    end();
+                };
+                client.info();
+                client.info(function () {});
             });
 
             it("emit error after a failure", function (done) {
