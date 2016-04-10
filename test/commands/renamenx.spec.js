6b4e7b5502b85de410f18448007e05d59c926834
Ben <benlowry@outlook.com>
2016-04-10 08:32:24 -0600

    Fixed function name case

--- test/commands/renamenx.spec.js
+++ test/commands/renamenx.spec.js
@@ -20,7 +20,7 @@ describe("The 'renamenx' method", function () {
 
             it('renames the key if target does not yet exist', function (done) {
                 client.set('foo', 'bar', helper.isString('OK'));
-                client.RENAMENX('foo', 'foo2', helper.isNumber(1));
+                client.renamenx('foo', 'foo2', helper.isNumber(1));
                 client.exists('foo', helper.isNumber(0));
                 client.exists(['foo2'], helper.isNumber(1, done));
             });
