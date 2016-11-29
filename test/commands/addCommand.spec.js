58d4921e27efd838a8a4124eb53db4876e79e386
Kyle Davis <halldirector@gmail.com>
2016-11-29 13:03:52 -0500

    original and special char commands + test update

--- test/commands/addCommand.spec.js
+++ test/commands/addCommand.spec.js
@@ -1,44 +1,36 @@
 'use strict';
 
 var config = require('../lib/config');
-var helper = require('../helper');
 var redis = config.redis;
 var assert = require('assert');
 
 describe("The 'addCommand/add_command' method", function () {
+    var client = redis.createClient();
+    var testCommands = {
+        newcommand  : 'newcommand',
+        nonJsSafe   : 'really-new.command',
+        jsSafe      : 'really_new_command'
+    };
 
-    helper.allTests(function (parser, ip, args) {
-
-        describe('using ' + parser + ' and ' + ip, function () {
-            var client;
-
-            beforeEach(function (done) {
-                client = redis.createClient.apply(null, args);
-                done();
-            });
-
-            it('camel case version exists', function (done) {
-                assert.strictEqual(typeof redis.addCommand, 'function');
-                return done(null);
-            });
-            it('snake version exists', function (done) {
-                assert.strictEqual(typeof redis.add_command, 'function');
-                return done(null);
-            });
-            it('generates a new method for an added command', function (done) {
-                redis.addCommand('newcommand');
-                assert.strictEqual(typeof client.newcommand, 'function');
-                return done(null);
-            });
-            it('converts illegal command names to JS-safe', function (done) {
-                redis.addCommand('really-new.command');
-                assert.strictEqual(typeof client.really_new_command, 'function');
-                return done(null);
-            });
-
-            afterEach(function () {
-                client.quit();
-            });
-        });
+    it('camel case version exists', function () {
+        assert.strictEqual(typeof redis.addCommand, 'function');
     });
+    it('snake version exists', function () {
+        assert.strictEqual(typeof redis.add_command, 'function');
+    });
+    it('does not already have the test standard command', function () {
+        assert.strictEqual(client[testCommands.newcommand], undefined);
+    });
+    it('generates a new method for an added command', function () {
+        redis.addCommand(testCommands.newcommand);
+        assert.strictEqual(typeof client[testCommands.newcommand], 'function');
+    });
+    it('does not already have the test non-JS-safe command', function () {
+        assert.strictEqual(client[testCommands.nonJsSafe], undefined);
+    });
+    it('converts illegal command names to JS-safe functions', function () {
+        redis.addCommand(testCommands.nonJsSafe);
+        assert.strictEqual(typeof client[testCommands.jsSafe], 'function');
+    });
+    client.quit();
 });
