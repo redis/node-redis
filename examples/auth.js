be3976e8ba5ae5af9241108c9fba835cf882d19f
Ruben Bridgewater <ruben@bridgewater.de>
2017-05-27 01:12:05 +0200

    doc: remove outdated examples

--- examples/auth.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const redis = require('redis')
-// The client stashes the password and will re-authenticate on every connect.
-redis.createClient({
-  password: 'some pass'
-})
--- examples/mget.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const client = require('redis').createClient()
-
-client.mget(['sessions started', 'sessions started', 'foo'], (err, res) => {
-  if (err) throw err
-  console.dir(res)
-})
