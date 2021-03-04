fee79451f3f282033ce315bfd30a69bbcc9785d9
leibale <leibale1998@gmail.com>
2021-03-04 18:57:24 -0500

    fix errors.subscribeUnsubscribeOnly regex

--- test/errors.js
+++ test/errors.js
@@ -2,5 +2,5 @@
 
 module.exports = {
     invalidPassword: /^(ERR invalid password|WRONGPASS invalid username-password pair)/,
-    subscribeUnsubscribeOnly: /^ERR( Can't execute 'get':) only \(P\)SUBSCRIBE \/ \(P\)UNSUBSCRIBE/
+    subscribeUnsubscribeOnly: /^ERR( Can't execute 'get':)? only \(P\)SUBSCRIBE \/ \(P\)UNSUBSCRIBE/
 };
