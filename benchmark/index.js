96ed4808dc868cb96581d9e85fb85e1710129b8e
leibale <leibale1998@gmail.com>
2021-06-02 13:43:12 -0400

    init benchmark workflow

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,57 +1,46 @@
-const oldRedis = require('redis-old'),
+const cronometro = require('cronometro'),
+    { once } = require('events'),
     newRedis = require('redis-new'),
-    {promisify} = require('util'),
-    {once} = require('events'),
-    ITERATIONS = 1_000_000;
+    oldRedis = require('redis-old');
 
-function benchmarkCallback(name, fn) {
-    return new Promise(resolve => {
-        const start = process.hrtime.bigint();
-
-        let counter = 0;
-
-        function iterationCallback(err) {
-            if (err) {
-                console.error(err);
-            }
-
-            if (++counter === ITERATIONS) {
-                console.log(`${name} took ${process.hrtime.bigint() - start} nanoseconds`);
-                resolve();
-            }
+let client;
+cronometro({
+    'New Client - Modern Mode': {
+        async before() {
+            client = newRedis.createClient();
+            await client.connect();
+        },
+        test() {
+            return client.ping();
+        },
+        after() {
+            return client.disconnect();
         }
-
-        for (let i = 0; i < ITERATIONS; i++) {
-            fn(iterationCallback);
+    },
+    'New Client - Legacy Mode': {
+        async before() {
+            client = newRedis.createClient({
+                legacyMode: true
+            });
+            await client.connect();
+        },
+        test(callback) {
+            client.ping(callback);
+        },
+        after() {
+            return client.disconnect();
         }
-    });
-}
-
-async function benchmarkPromise(name, fn) {
-    const start = process.hrtime.bigint();
-
-    const promises = [];
-    for (let i = 0; i < ITERATIONS; i++) {
-        promises.push(fn());
+    },
+    'Old Client': {
+        before(callback) {
+            client = oldRedis.createClient();
+            client.once('ready', callback)
+        },
+        test(callback) {
+            client.ping(callback);
+        },
+        after(callback) {
+            client.quit(callback);
+        }
     }
-
-    await Promise.all(promises);
-    console.log(`${name} took ${process.hrtime.bigint() - start} nanoseconds`);
-}
-
-(async () => {
-    const oldClient = oldRedis.createClient();
-    oldClient.flushallAsync = promisify(oldClient.flushall).bind(oldClient);
-    await once(oldClient, 'connect');
-    await oldClient.flushallAsync();
-    await benchmarkCallback('oldClient.ping', cb => {
-        oldClient.ping('key', cb);
-    });
-
-    const newClient = newRedis.createClient();
-    await newClient.connect();
-    await newClient.flushAll();
-    await benchmarkPromise('newClient.ping', () => {
-        return newClient.ping();
-    });
-})();
+});
