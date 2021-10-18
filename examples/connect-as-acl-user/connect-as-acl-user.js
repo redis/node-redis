26774f8fa3700623699d7f30361f4c5b7a699e1d
Simon Prickett <simon@redislabs.com>
2021-10-18 23:20:00 +0100

    Adds connect-as-acl-user example. (#1684)
    * Adds connect-as-acl-user example.
    
    * Adds blank line at end.
    
    * Set to private.
    
    * Adds examples folder to npmignore.

---
+++ examples/connect-as-acl-user/connect-as-acl-user.js
@@ -0,0 +1,32 @@
+// Connect to Redis 6.x as an ACL user.  Attempt to run a command
+// that the user is allowed to execute, and a command that the 
+// user is not allowed to execute.
+
+// Create the test user in redis-cli with this command:
+//   acl setuser testuser on >testpassword +ping
+
+import { createClient } from 'redis';
+
+const connectWithACLUser = async () => {
+  const client = createClient({
+    url: 'redis://testuser:testpassword@127.0.0.1:6379'
+  });
+
+  await client.connect();
+
+  // Returns PONG
+  console.log(`Response from PING command: ${await client.ping()}`);
+
+  try {
+    // This will error as this user is not allowed to run this command...
+    console.log(`Response from GET command: ${await client.get('somekey')}`);
+  } catch (e) {
+    console.log(`GET command failed: ${e.message}`);
+  }
+
+  await client.quit();
+}
+
+
+connectWithACLUser();
+
