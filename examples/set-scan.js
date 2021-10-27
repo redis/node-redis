d409120e5be6e96a3d5d6efef1e4532f48b45182
Kalki <shashank.kviit@gmail.com>
2021-10-28 02:24:12 +0530

    Issue # 1697 FIX - creates an example script that shows how to use the SSCAN iterator (#1699)
    * #1697 fix for set scan example
    
    * adds the js file
    
    * adds comment
    
    * Minor layout and comment adjustment.
    
    Co-authored-by: srawat2 <shashank19aug>
    Co-authored-by: Simon Prickett <simon@redislabs.com>
    
    Closes #1697.

---
+++ examples/set-scan.js
@@ -0,0 +1,19 @@
+// An example script that shows how to use the SSCAN iterator functionality to retrieve the contents of a Redis set.
+// Create the set in redis-cli with this command:
+//     sadd setName a b c d e f g h i j k l m n o p q
+
+import { createClient } from 'redis';
+
+async function setScan() {
+  const client = createClient();
+  await client.connect();
+
+  const setName = 'setName';
+  for await (const member of client.sScanIterator(setName)) {
+    console.log(member);
+  }
+
+  await client.quit();
+}
+
+setScan();
