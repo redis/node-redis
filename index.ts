994d8dcbe1300aa9096676b171c1ce7e739320f3
leibale <leibale1998@gmail.com>
2021-11-15 16:00:28 -0500

    update readmes, add createCluster to the `redis` package

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-import { createClient as _createClient } from '@redis/client';
+import { createClient as _createClient, createCluster as _createCluster } from '@redis/client';
 import { RedisScripts } from '@redis/client/dist/lib/commands';
 import { RedisClientOptions, RedisClientType } from '@redis/client/dist/lib/client';
+import { RedisClusterOptions, RedisClusterType } from '@redis/client/dist/lib/cluster';
 import RedisJSON from '@redis/json';
 import RediSearch from '@redis/search';
 
+export * from '@redis/client';
+export * from '@redis/json';
 export * from '@redis/search';
 
 const modules =  {
@@ -15,6 +18,15 @@ export function createClient<S extends RedisScripts = Record<string, never>>(
     options?: Omit<RedisClientOptions<never, S>, 'modules'>
 ): RedisClientType<typeof modules, S> {
     return _createClient({
+        ...options,
+        modules
+    });
+}
+
+export function createCluster<S extends RedisScripts = Record<string, never>>(
+    options: Omit<RedisClusterOptions<never, S>, 'modules'>
+): RedisClusterType<typeof modules, S> {
+    return _createCluster({
         ...options,
         modules
     });
--- packages/json/README.md
+++ packages/json/README.md
@@ -1,2 +1,2 @@
-# redis
+# @redis/json
 The sources and docs for this package are in the main [node-redis](https://github.com/redis/node-redis) repo.
