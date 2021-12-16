9fceee4274deef7ee906f1f72a962d5b5617ecc6
AvitalFineRedis <avital.fine@redislabs.com>
2021-12-16 10:49:44 +0100

    small changes

--- packages/bloom/lib/commands/bloom/MEXISTS.spec.ts
+++ packages/bloom/lib/commands/bloom/MEXISTS.spec.ts
@@ -1,19 +1,22 @@
-// import { strict as assert } from 'assert';
-// import testUtils, { GLOBAL } from '../../test-utils';
-// import { transformArguments } from './MEXISTS';
+import { strict as assert } from 'assert';
+import testUtils, { GLOBAL } from '../../test-utils';
+import { transformArguments } from './MEXISTS';
 
-// describe('BF MEXISTS', () => {
-//     describe('transformArguments', () => {
-//         it('basic add', () => {
-//             assert.deepEqual(
-//                 transformArguments('BLOOM', 'foo', 'bar'),
-//                 ['BF.MEXISTS', 'BLOOM', 'foo', 'bar']
-//             );
-//         });
-//     });
+describe('BF MEXISTS', () => {
+    describe('transformArguments', () => {
+        it('basic add', () => {
+            assert.deepEqual(
+                transformArguments('BLOOM', 'foo', 'bar'),
+                ['BF.MEXISTS', 'BLOOM', 'foo', 'bar']
+            );
+        });
+    });
 
-//     testUtils.testWithClient('client.bf.exists', async client => {
-//         await client.bf.add('BLOOM', 'foo'); 
-//         assert.deepEqual(await client.bf.mExists('BLOOM', 'foo', 'bar'), [1,0]);
-//     }, GLOBAL.SERVERS.OPEN);
-// });
+    testUtils.testWithClient('client.bf.exists', async client => {
+        await client.bf.add('BLOOM', 'foo');
+        assert.deepEqual(
+            await client.bf.mExists('BLOOM', 'foo', 'bar'), 
+            [true, false]
+        );
+    }, GLOBAL.SERVERS.OPEN);
+});
