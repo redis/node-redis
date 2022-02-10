f8dacb743b4cce9767e5e20a4924e45672cf9dfc
leibale <leibale1998@gmail.com>
2022-02-10 14:55:36 -0500

    improve performance

--- packages/client/lib/client/RESP2/composers/buffer.ts
+++ packages/client/lib/client/RESP2/composers/buffer.ts
@@ -1,14 +1,14 @@
 import { Composer } from './interface';
 
 export default class BufferComposer implements Composer<Buffer> {
-    #chunks: Array<Buffer> = [];
+    private chunks: Array<Buffer> = [];
 
     write(buffer: Buffer): void {
-        this.#chunks.push(buffer);
+        this.chunks.push(buffer);
     }
 
     end(buffer: Buffer): Buffer {
         this.write(buffer);
-        return Buffer.concat(this.#chunks.splice(0));
+        return Buffer.concat(this.chunks.splice(0));
     }
 }
--- packages/client/lib/client/RESP2/composers/string.ts
+++ packages/client/lib/client/RESP2/composers/string.ts
@@ -2,17 +2,17 @@ import { StringDecoder } from 'string_decoder';
 import { Composer } from './interface';
 
 export default class StringComposer implements Composer<string> {
-    #decoder = new StringDecoder();
+    private decoder = new StringDecoder();
 
-    #string = '';
+    private string = '';
 
     write(buffer: Buffer): void {
-        this.#string += this.#decoder.write(buffer);
+        this.string += this.decoder.write(buffer);
     }
 
     end(buffer: Buffer): string {
-        const string = this.#string + this.#decoder.end(buffer);
-        this.#string = '';
+        const string = this.string + this.decoder.end(buffer);
+        this.string = '';
         return string;
     }
 }
