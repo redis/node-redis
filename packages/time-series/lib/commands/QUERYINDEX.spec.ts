21ef76ee1830c76128b7d8038e7ad2c45d032037
Jimmy <jgdames@gmail.com>
2022-02-18 22:19:23 -0600

    fix TS.QUERYINDEX with multiple filters

--- packages/time-series/lib/commands/QUERYINDEX.spec.ts
+++ packages/time-series/lib/commands/QUERYINDEX.spec.ts
@@ -10,11 +10,24 @@ describe('QUERYINDEX', () => {
         );
     });
 
+    it('transformArguments multiple', () => {
+        assert.deepEqual(
+            transformArguments(['a', 'b', 'c']),
+            ['TS.QUERYINDEX', 'a', 'b', 'c']
+        );
+    });
+
     testUtils.testWithClient('client.ts.queryIndex', async client => {
         await Promise.all([
             client.ts.create('key', {
                 LABELS: {
                     label: 'value'
+                }
+            }),
+            client.ts.create('key2', {
+                LABELS: {
+                    label1: 'value1',
+                    label2: 'value2'
                 }
             })
         ]);
@@ -22,6 +35,10 @@ describe('QUERYINDEX', () => {
         assert.deepEqual(
             await client.ts.queryIndex('label=value'),
             ['key']
+        );
+        assert.deepEqual(
+            await client.ts.queryIndex(['label1=value1', 'label2=value2']),
+            ['key2']
         );
     }, GLOBAL.SERVERS.OPEN);
 });
