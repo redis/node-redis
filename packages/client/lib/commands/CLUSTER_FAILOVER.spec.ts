e88cfdaf00a850e098de1b981f1638d301ae1e66
Avital-Fine <avital.fine@redis.com>
2022-03-01 13:20:23 +0100

    delete reset clietn test

--- packages/client/lib/commands/CLUSTER_FAILOVER.spec.ts
+++ packages/client/lib/commands/CLUSTER_FAILOVER.spec.ts
@@ -16,5 +16,4 @@ describe('CLUSTER FAILOVER', () => {
             );
         });
     });
-
 });
--- packages/client/lib/commands/CLUSTER_RESET.spec.ts
+++ packages/client/lib/commands/CLUSTER_RESET.spec.ts
@@ -1,5 +1,4 @@
 import { strict as assert } from 'assert';
-import testUtils, { GLOBAL } from '../test-utils';
 import { transformArguments } from './CLUSTER_RESET';
 
 describe('CLUSTER RESET', () => {
