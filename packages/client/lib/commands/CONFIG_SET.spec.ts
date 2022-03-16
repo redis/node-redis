31b5a6f90fab7ff4841085643bbb9480830a007f
Avital-Fine <avital.fine@redis.com>
2022-03-16 13:49:53 +0100

    support multiple parametrs on CONFIG SET

--- packages/client/lib/commands/CONFIG_SET.spec.ts
+++ packages/client/lib/commands/CONFIG_SET.spec.ts
@@ -2,10 +2,19 @@ import { strict as assert } from 'assert';
 import { transformArguments } from './CONFIG_SET';
 
 describe('CONFIG SET', () => {
-    it('transformArguments', () => {
-        assert.deepEqual(
-            transformArguments('parameter', 'value'),
-            ['CONFIG', 'SET', 'parameter', 'value']
-        );
+    describe('transformArguments', () => {
+        it('set one parameter (old version)', () => {
+            assert.deepEqual(
+                transformArguments('parameter', 'value'),
+                ['CONFIG', 'SET', 'parameter', 'value']
+            );
+        });
+
+        it('set muiltiple parameters', () => {
+            assert.deepEqual(
+                transformArguments('parameter', 'value', [['parameter2', 'value2'], ['parameter3', 'value3']]),
+                ['CONFIG', 'SET', 'parameter', 'value', 'parameter2', 'value2', 'parameter3', 'value3']
+            );
+        });
     });
 });
--- packages/client/lib/commands/CONFIG_SET.ts
+++ packages/client/lib/commands/CONFIG_SET.ts
@@ -1,5 +1,15 @@
-export function transformArguments(parameter: string, value: string): Array<string> {
-    return ['CONFIG', 'SET', parameter, value];
+export function transformArguments(
+    parameter: string,
+    value: string,
+    mulitipleParams?: Array<[parameter: string, value: string]>
+): Array<string> {
+    const args = ['CONFIG', 'SET', parameter, value];
+
+    if (mulitipleParams) {
+        mulitipleParams.forEach(pair => args.push(pair[0], pair[1]));
+    }
+
+    return args;
 }
 
 export declare function transformReply(): string;
