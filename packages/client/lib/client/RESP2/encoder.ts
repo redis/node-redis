3ec17e31b32bd919400ef271c39fab28e0809b0d
Leibale Eidelman <leibale1998@gmail.com>
2022-05-11 14:20:26 -0400

    fix legacy mode resp encoder (#2118)
    * fix legacy mode resp encoder
    
    * Update encoder.ts

--- packages/client/lib/client/RESP2/encoder.ts
+++ packages/client/lib/client/RESP2/encoder.ts
@@ -5,23 +5,21 @@ const CRLF = '\r\n';
 export default function encodeCommand(args: RedisCommandArguments): Array<RedisCommandArgument> {
     const toWrite: Array<RedisCommandArgument> = [];
 
-    let strings = `*${args.length}${CRLF}`;
+    let strings = '*' + args.length + CRLF;
 
     for (let i = 0; i < args.length; i++) {
         const arg = args[i];
         if (typeof arg === 'string') {
-            const byteLength = Buffer.byteLength(arg);
-            strings += `$${byteLength}${CRLF}`;
-            strings += arg;
+            strings += '$' + Buffer.byteLength(arg) + CRLF + arg + CRLF;
         } else if (arg instanceof Buffer) {
-            toWrite.push(`${strings}$${arg.length}${CRLF}`);
-            strings = '';
-            toWrite.push(arg);
+            toWrite.push(
+                strings + '$' + arg.length.toString() + CRLF,
+                arg
+            );
+            strings = CRLF;
         } else {
             throw new TypeError('Invalid argument type');
         }
-
-        strings += CRLF;
     }
 
     toWrite.push(strings);
