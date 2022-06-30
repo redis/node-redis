be33ffbd0f1ffb2ad68bc5d756a0f8775616d865
leibale <leibale1998@gmail.com>
2022-06-30 12:47:17 -0400

    upgrade deps, fix "invalid save argument" error

--- packages/json/lib/test-utils.ts
+++ packages/json/lib/test-utils.ts
@@ -4,7 +4,7 @@ import RedisJSON from '.';
 export default new TestUtils({
     dockerImageName: 'redislabs/rejson',
     dockerImageVersionArgument: 'rejson-version',
-    defaultDockerVersion: '2.0.7'
+    defaultDockerVersion: '2.0.9'
 });
 
 export const GLOBAL = {
