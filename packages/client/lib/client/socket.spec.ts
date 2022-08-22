942de1f0b4868f0f6464b2e0702b621a3373c4ee
Leibale Eidelman <leibale1998@gmail.com>
2022-08-22 17:59:45 -0400

    Handle unhandled errors in `socket.reconnectStrategry` (#2226)
    * handle errors in reconnect strategy
    
    * add test
    
    * fix retries typo
    
    * fix #2237 - flush queues on reconnect strategy error
    
    * Update socket.ts
    
    * Update socket.ts

--- packages/client/lib/client/socket.spec.ts
+++ packages/client/lib/client/socket.spec.ts
@@ -1,41 +1,63 @@
 import { strict as assert } from 'assert';
 import { SinonFakeTimers, useFakeTimers, spy } from 'sinon';
-import RedisSocket from './socket';
+import RedisSocket, { RedisSocketOptions } from './socket';
 
 describe('Socket', () => {
+    function createSocket(options: RedisSocketOptions): RedisSocket {
+        const socket = new RedisSocket(
+            () => Promise.resolve(),
+            options
+        );
+
+        socket.on('error', (err) => {
+            // ignore errors
+            console.log(err);
+        });
+
+        return socket;
+    }
+
     describe('reconnectStrategy', () => {
         let clock: SinonFakeTimers;
         beforeEach(() => clock = useFakeTimers());
         afterEach(() => clock.restore());
 
-        it('custom strategy', () => {
-            const reconnectStrategy = spy((retries: number): number | Error => {
+        it('custom strategy', async () => {
+            const reconnectStrategy = spy((retries: number) => {
                 assert.equal(retries + 1, reconnectStrategy.callCount);
 
-                if (retries === 50) {
-                    return Error('50');
-                }
+                if (retries === 50) return new Error('50');
 
                 const time = retries * 2;
                 queueMicrotask(() => clock.tick(time));
                 return time;
             });
 
-            const socket = new RedisSocket(
-                () => Promise.resolve(),
-                {
-                    host: 'error',
-                    reconnectStrategy
-                }
-            );
-
-            socket.on('error', () => {
-                // ignore errors
+            const socket = createSocket({
+                host: 'error',
+                reconnectStrategy
             });
 
-            return assert.rejects(socket.connect(), {
+            await assert.rejects(socket.connect(), {
                 message: '50'
             });
+
+            assert.equal(socket.isOpen, false);
+        });
+
+        it('should handle errors', async () => {
+            const socket = createSocket({
+                host: 'error',
+                reconnectStrategy(retries: number) {
+                    if (retries === 1) return new Error('done');
+                    queueMicrotask(() => clock.tick(500));
+                    throw new Error();
+                }
+            });
+
+            await assert.rejects(socket.connect());
+
+            assert.equal(socket.isOpen, false);
         });
     });
 });
