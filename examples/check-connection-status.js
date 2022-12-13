e4e242bf389494e18627f6c66f5ef98852963e15
Simon Prickett <simon@redislabs.com>
2022-12-13 20:01:45 +0000

    Improved example.

--- examples/check-connection-status.js
+++ examples/check-connection-status.js
@@ -6,20 +6,23 @@ const client = createClient();
 console.log('Before client.connect()...');
 
 // isOpen will return False here as the client's socket is not open yet.
-console.log(`client.isOpen: ${client.isOpen}`);
-
 // isReady will return False here, client is not yet ready to use.
-console.log(`client.isReady: ${client.isReady}`);
+console.log(`client.isOpen: ${client.isOpen}, client.isReady: ${client.isReady}`);
 
-// Connect...
-await client.connect();
+// Begin connection process...
+const connectPromise = client.connect();
 
 console.log('After client.connect()...');
 
 // isOpen will return True here as the client's socket is open now.
-console.log(`client.isOpen: ${client.isOpen}`);
+// isReady will return False here as the promise hasn't resolved yet.
+console.log(`client.isOpen: ${client.isOpen}, client.isReady: ${client.isReady}`);
 
+await connectPromise;
+console.log('Afer connectPromise has resolved...');
+
+// isOpen will return True here as the client's socket is open now.
 // isReady will return True here, client is ready to use.
-console.log(`client.isReady: ${client.isReady}`);
+console.log(`client.isOpen: ${client.isOpen}, client.isReady: ${client.isReady}`);
 
 await client.quit();
