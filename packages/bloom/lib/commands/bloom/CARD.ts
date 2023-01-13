4490cf12947df65b0697b8c47ff419361db8dbac
Leibale Eidelman <me@leibale.com>
2023-01-13 01:10:55 -0500

    Update CARD.ts

--- packages/bloom/lib/commands/bloom/CARD.ts
+++ packages/bloom/lib/commands/bloom/CARD.ts
@@ -5,3 +5,5 @@ export const IS_READ_ONLY = true;
 export function transformArguments(key: string): Array<string> {
     return ['BF.CARD', key];
 }
+
+export declare function transformReply(): number;
