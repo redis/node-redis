aae501e271416fafedd76ba6a58600f5130dbb9c
Leibale <me@leibale.com>
2023-04-26 14:29:04 -0400

    wip

--- packages/redis/index.ts
+++ packages/redis/index.ts
@@ -1,13 +1,13 @@
 import {
-    RedisModules,
-    RedisFunctions,
-    RedisScripts,
-    createClient as _createClient,
-    RedisClientOptions,
-    RedisClientType as _RedisClientType,
-    createCluster as _createCluster,
-    RedisClusterOptions,
-    RedisClusterType as _RedisClusterType
+  RedisModules,
+  RedisFunctions,
+  RedisScripts,
+  createClient as _createClient,
+  RedisClientOptions,
+  RedisClientType as _RedisClientType,
+  createCluster as _createCluster,
+  RedisClusterOptions,
+  RedisClusterType as _RedisClusterType
 } from '@redis/client';
 import RedisBloomModules from '@redis/bloom';
 import RedisGraph from '@redis/graph';
@@ -23,55 +23,55 @@ export * from '@redis/search';
 export * from '@redis/time-series';
 
 const modules = {
-    ...RedisBloomModules,
-    graph: RedisGraph,
-    json: RedisJSON,
-    ft: RediSearch,
-    ts: RedisTimeSeries
+  ...RedisBloomModules,
+  graph: RedisGraph,
+  json: RedisJSON,
+  ft: RediSearch,
+  ts: RedisTimeSeries
 };
 
 export type RedisDefaultModules = typeof modules;
 
 export type RedisClientType<
-    M extends RedisModules = RedisDefaultModules,
-    F extends RedisFunctions = Record<string, never>,
-    S extends RedisScripts = Record<string, never>
+  M extends RedisModules = RedisDefaultModules,
+  F extends RedisFunctions = Record<string, never>,
+  S extends RedisScripts = Record<string, never>
 > = _RedisClientType<M, F, S>;
 
 export function createClient<
-    M extends RedisModules,
-    F extends RedisFunctions,
-    S extends RedisScripts
+  M extends RedisModules,
+  F extends RedisFunctions,
+  S extends RedisScripts
 >(
-    options?: RedisClientOptions<M, F, S>
+  options?: RedisClientOptions<M, F, S>
 ): _RedisClientType<RedisDefaultModules & M, F, S> {
-    return _createClient({
-        ...options,
-        modules: {
-            ...modules,
-            ...(options?.modules as M)
-        }
-    });
+  return _createClient({
+    ...options,
+    modules: {
+      ...modules,
+      ...(options?.modules as M)
+    }
+  });
 }
 
 export type RedisClusterType<
-    M extends RedisModules = RedisDefaultModules,
-    F extends RedisFunctions = Record<string, never>,
-    S extends RedisScripts = Record<string, never>
+  M extends RedisModules = RedisDefaultModules,
+  F extends RedisFunctions = Record<string, never>,
+  S extends RedisScripts = Record<string, never>
 > = _RedisClusterType<M, F, S>;
 
 export function createCluster<
-    M extends RedisModules,
-    F extends RedisFunctions,
-    S extends RedisScripts
+  M extends RedisModules,
+  F extends RedisFunctions,
+  S extends RedisScripts
 >(
-    options: RedisClusterOptions<M, F, S>
+  options: RedisClusterOptions<M, F, S>
 ): RedisClusterType<RedisDefaultModules & M, F, S> {
-    return _createCluster({
-        ...options,
-        modules: {
-            ...modules,
-            ...(options?.modules as M)
-        }
-    });
+  return _createCluster({
+    ...options,
+    modules: {
+      ...modules,
+      ...(options?.modules as M)
+    }
+  });
 }
