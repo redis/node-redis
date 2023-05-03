642c823d1b3cc5b1d0e837ed142dae6af7697ee6
dovi <dovi.rivkin@gmail.com>
2023-05-03 14:50:33 -0400

    fix DECRBY

--- packages/client/lib/commands/DECRBY.ts
+++ packages/client/lib/commands/DECRBY.ts
@@ -3,7 +3,7 @@ import { RedisArgument, NumberReply, Command } from '../RESP/types';
 export default {
   FIRST_KEY_INDEX: 1,
   transformArguments(key: RedisArgument, decrement: number) {
-    return ['DECR', key, decrement.toString()];
+    return ['DECRBY', key, decrement.toString()];
   },
   transformReply: undefined as unknown as () => NumberReply
 } as const satisfies Command;
