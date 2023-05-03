c6b21eab520c32a38a799160a5af039eaf424770
dovi <dovi.rivkin@gmail.com>
2023-05-03 15:02:40 -0400

    fix GETEX spec

--- packages/client/lib/commands/GETEX.spec.ts
+++ packages/client/lib/commands/GETEX.spec.ts
@@ -1,96 +1,90 @@
 import { strict as assert } from 'assert';
 import testUtils, { GLOBAL } from '../test-utils';
-import { transformArguments } from './GETEX';
+import GETEX from './GETEX';
 
 describe('GETEX', () => {
-    testUtils.isVersionGreaterThanHook([6, 2]);
+  testUtils.isVersionGreaterThanHook([6, 2]);
 
-    describe('transformArguments', () => {
-        it('EX', () => {
-            assert.deepEqual(
-                transformArguments('key', {
-                    EX: 1
-                }),
-                ['GETEX', 'key', 'EX', '1']
-            );
-        });
-
-        it('PX', () => {
-            assert.deepEqual(
-                transformArguments('key', {
-                    PX: 1
-                }),
-                ['GETEX', 'key', 'PX', '1']
-            );
-        });
-
-        describe('EXAT', () => {
-            it('number', () => {
-                assert.deepEqual(
-                    transformArguments('key', {
-                        EXAT: 1
-                    }),
-                    ['GETEX', 'key', 'EXAT', '1']
-                );
-            });
-
-            it('date', () => {
-                const d = new Date();
-                assert.deepEqual(
-                    transformArguments('key', {
-                        EXAT: d
-                    }),
-                    ['GETEX', 'key', 'EXAT', Math.floor(d.getTime() / 1000).toString()]
-                );
-            });
-        });
-
-        describe('PXAT', () => {
-            it('number', () => {
-                assert.deepEqual(
-                    transformArguments('key', {
-                        PXAT: 1
-                    }),
-                    ['GETEX', 'key', 'PXAT', '1']
-                );
-            });
-
-            it('date', () => {
-                const d = new Date();
-                assert.deepEqual(
-                    transformArguments('key', {
-                        PXAT: d
-                    }),
-                    ['GETEX', 'key', 'PXAT', d.getTime().toString()]
-                );
-            });
-        });
-
-        it('PERSIST', () => {
-            assert.deepEqual(
-                transformArguments('key', {
-                    PERSIST: true
-                }),
-                ['GETEX', 'key', 'PERSIST']
-            );
-        });
+  describe('transformArguments', () => {
+    it('EX', () => {
+      assert.deepEqual(
+        GETEX.transformArguments('key', {
+          EX: 1
+        }),
+        ['GETEX', 'key', 'EX', '1']
+      );
     });
 
-    testUtils.testWithClient('client.getEx', async client => {
-        assert.equal(
-            await client.getEx('key', {
-                PERSIST: true
-            }),
-            null
-        );
-    }, GLOBAL.SERVERS.OPEN);
+    it('PX', () => {
+      assert.deepEqual(
+        GETEX.transformArguments('key', {
+          PX: 1
+        }),
+        ['GETEX', 'key', 'PX', '1']
+      );
+    });
 
-    testUtils.testWithCluster('cluster.getEx', async cluster => {
-        assert.equal(
-            await cluster.getEx('key', {
-                PERSIST: true
-            }),
-            null
+    describe('EXAT', () => {
+      it('number', () => {
+        assert.deepEqual(
+          GETEX.transformArguments('key', {
+            EXAT: 1
+          }),
+          ['GETEX', 'key', 'EXAT', '1']
         );
-    }, GLOBAL.CLUSTERS.OPEN);
+      });
+
+      it('date', () => {
+        const d = new Date();
+        assert.deepEqual(
+          GETEX.transformArguments('key', {
+            EXAT: d
+          }),
+          ['GETEX', 'key', 'EXAT', Math.floor(d.getTime() / 1000).toString()]
+        );
+      });
+    });
+
+    describe('PXAT', () => {
+      it('number', () => {
+        assert.deepEqual(
+          GETEX.transformArguments('key', {
+            PXAT: 1
+          }),
+          ['GETEX', 'key', 'PXAT', '1']
+        );
+      });
+
+      it('date', () => {
+        const d = new Date();
+        assert.deepEqual(
+          GETEX.transformArguments('key', {
+            PXAT: d
+          }),
+          ['GETEX', 'key', 'PXAT', d.getTime().toString()]
+        );
+      });
+    });
+
+    it('PERSIST', () => {
+      assert.deepEqual(
+        GETEX.transformArguments('key', {
+          PERSIST: true
+        }),
+        ['GETEX', 'key', 'PERSIST']
+      );
+    });
+  });
+
+  testUtils.testAll('getEx', async client => {
+    assert.equal(
+      await client.getEx('key', {
+        PERSIST: true
+      }),
+      null
+    );
+  }, {
+    client: GLOBAL.SERVERS.OPEN,
+    cluster: GLOBAL.CLUSTERS.OPEN
+  });
 });
