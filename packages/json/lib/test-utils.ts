cee764e5a193c2dfc1e29c4ea1c9cd8e88faa7cb
shacharPash <shachar.pashchur@redis.com>
2023-05-16 17:03:38 +0300

    test on edge

--- packages/json/lib/test-utils.ts
+++ packages/json/lib/test-utils.ts
@@ -3,7 +3,8 @@ import RedisJSON from '.';
 
 export default new TestUtils({
     dockerImageName: 'redislabs/rejson',
-    dockerImageVersionArgument: 'rejson-version'
+    dockerImageVersionArgument: 'rejson-version',
+    defaultDockerVersion: 'edge'
 });
 
 export const GLOBAL = {
