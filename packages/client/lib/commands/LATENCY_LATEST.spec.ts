4292a345a5d94658c7230da1c10444916776d4f1
Kumar Arnav <36244620+Karnav123@users.noreply.github.com>
2023-05-25 08:49:13 +0000

    Fix the review comments.

--- packages/client/lib/commands/LATENCY_LATEST.spec.ts
+++ packages/client/lib/commands/LATENCY_LATEST.spec.ts
@@ -17,11 +17,11 @@ describe('LATENCY LATEST', () => {
         ]);
         const latency = await client.latencyLatest();
         assert.ok(Array.isArray(latency));
-        for (const event of latency) {
-            assert.ok(typeof event.name === 'string', 'Name should be a string');
-            assert.ok(typeof event.timestamp === 'number', 'Timestamp should be a number');
-            assert.ok(typeof event.latestLatency === 'number', 'Latest latency should be a number');
-            assert.ok(typeof event.allTimeLatency === 'number', 'All-time latency should be a number');
+        for (const [name, timestamp, latestLatency, allTimeLatency] of latency) {
+            assert.equal(typeof name, 'string');
+            assert.equal(typeof timestamp, 'number');
+            assert.equal(typeof latestLatency, 'number');
+            assert.equal(typeof allTimeLatency, 'number');
         }
     }, GLOBAL.SERVERS.OPEN);
 });
