d4e2ab5f601086fa31da476566fbcac8a7838c55
Leibale <me@leibale.com>
2023-06-19 18:16:41 -0400

    wip

--- packages/client/lib/commands/LATENCY_LATEST.ts
+++ packages/client/lib/commands/LATENCY_LATEST.ts
@@ -1,12 +1,12 @@
-import { RedisCommandArguments } from '.';
+// import { RedisCommandArguments } from '.';
 
-export function transformArguments(): RedisCommandArguments {
-    return ['LATENCY', 'LATEST'];
-}
+// export function transformArguments(): RedisCommandArguments {
+//     return ['LATENCY', 'LATEST'];
+// }
 
-export declare function transformReply(): Array<[
-    name: string,
-    timestamp: number,
-    latestLatency: number,
-    allTimeLatency: number
-]>;
+// export declare function transformReply(): Array<[
+//     name: string,
+//     timestamp: number,
+//     latestLatency: number,
+//     allTimeLatency: number
+// ]>;
