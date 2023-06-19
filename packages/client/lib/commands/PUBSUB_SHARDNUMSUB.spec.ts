c277ba35c4bd61d3dd1431350b21e7e393f3dc84
Asher <asher@channel.io>
2023-06-20 01:20:10 +0900

    Add support for command 'PUBSUB SHARDNUMSUB'

---
+++ packages/client/lib/commands/PUBSUB_SHARDNUMSUB.spec.ts
@@ -0,0 +1,35 @@
+import { strict as assert } from 'assert';
+import testUtils, { GLOBAL } from '../test-utils';
+import { transformArguments } from './PUBSUB_NUMSUB';
+
+describe('PUBSUB SHARDNUMSUB', () => {
+    describe('transformArguments', () => {
+        it('simple', () => {
+            assert.deepEqual(
+                transformArguments(),
+                ['PUBSUB', 'SHARDNUMSUB']
+            );
+        });
+
+        it('string', () => {
+            assert.deepEqual(
+                transformArguments('channel'),
+                ['PUBSUB', 'SHARDNUMSUB', 'channel']
+            );
+        });
+
+        it('array', () => {
+            assert.deepEqual(
+                transformArguments(['1', '2']),
+                ['PUBSUB', 'SHARDNUMSUB', '1', '2']
+            );
+        });
+    });
+
+    testUtils.testWithClient('client.pubSubShardNumSub', async client => {
+        assert.deepEqual(
+            await client.pubSubShardNumSub(),
+            Object.create(null)
+        );
+    }, GLOBAL.SERVERS.OPEN);
+});
