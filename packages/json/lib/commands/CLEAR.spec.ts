0ed4ee41f441d7bc833ed98c5e884ff8b8ee584d
dovi <dovi.rivkin@gmail.com>
2023-07-05 17:42:39 -0400

    create JSON.CLEAR

---
+++ packages/json/lib/commands/CLEAR.spec.ts
@@ -0,0 +1,28 @@
+import { strict as assert } from 'assert';
+import testUtils, { GLOBAL } from '../test-utils';
+import CLEAR from './CLEAR';
+
+describe('CLEAR', () => {
+  describe('transformArguments', () => {
+    it('key', () => {
+      assert.deepEqual(
+        CLEAR.transformArguments('key'),
+        ['JSON.CLEAR', 'key']
+      );
+    });
+
+    it('key, path', () => {
+      assert.deepEqual(
+        CLEAR.transformArguments('key', '$.path'),
+        ['JSON.CLEAR', 'key', '$.path']
+      );
+    });
+  });
+
+  testUtils.testWithClient('client.json.clear', async client => {
+    assert.deepEqual(
+      await client.json.clear('key'),
+      0
+    );
+  }, GLOBAL.SERVERS.OPEN);
+});
---
+++ packages/json/lib/commands/CLEAR.ts
@@ -0,0 +1,14 @@
+import { RedisArgument, NumberReply, Command } from '@redis/client/dist/lib/RESP/types';
+
+export default {
+  FIRST_KEY_INDEX: 1,
+  IS_READ_ONLY: false,
+  transformArguments(key: RedisArgument, path?: RedisArgument) {
+    const args = ['JSON.CLEAR', key];
+
+    if (path) args.push(path);
+
+    return args;
+  },
+  transformReply: undefined as unknown as () => NumberReply
+} as const satisfies Command;
