a9ec9e2b9b0dfcc6e7c515b22adf246278606d79
dovi <dovi.rivkin@gmail.com>
2023-07-05 14:41:38 -0400

    fix DELETERULE

--- packages/time-series/lib/commands/DELETERULE.ts
+++ packages/time-series/lib/commands/DELETERULE.ts
@@ -1,11 +1,14 @@
-export const FIRST_KEY_INDEX = 1;
+import { RedisArgument, SimpleStringReply, Command } from '@redis/client/dist/lib/RESP/types';
 
-export function transformArguments(sourceKey: string, destinationKey: string): Array<string> {
+export default {
+  FIRST_KEY_INDEX: 1,
+  IS_READ_ONLY: false,
+  transformArguments(sourceKey: RedisArgument, destinationKey: RedisArgument) {
     return [
-        'TS.DELETERULE',
-        sourceKey,
-        destinationKey
+      'TS.DELETERULE',
+      sourceKey,
+      destinationKey
     ];
-}
-
-export declare function transformReply(): 'OK';
+  },
+  transformReply: undefined as unknown as () => SimpleStringReply<'OK'>
+} as const satisfies Command;
