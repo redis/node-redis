c0d9ed84a8d727b2eabec06740d9d80727bbff71
Leibale Eidelman <me@leibale.com>
2023-09-18 18:32:47 -0400

    clean code

--- packages/client/lib/commands/LATENCY_HISTORY.ts
+++ packages/client/lib/commands/LATENCY_HISTORY.ts
@@ -1,25 +1,23 @@
-import { RedisCommandArguments } from ".";
+export type EventType = (
+    'active-defrag-cycle' |
+    'aof-fsync-always' |
+    'aof-stat' |
+    'aof-rewrite-diff-write' |
+    'aof-rename' |
+    'aof-write' |
+    'aof-write-active-child' |
+    'aof-write-alone' |
+    'aof-write-pending-fsync' |
+    'command' |
+    'expire-cycle' |
+    'eviction-cycle' |
+    'eviction-del' |
+    'fast-command' |
+    'fork' |
+    'rdb-unlink-temp-file'
+);
 
-export type EventType =
-    'active-defrag-cycle'
-    | 'aof-fsync-always'
-    | 'aof-stat'
-    | 'aof-rewrite-diff-write'
-    | 'aof-rename'
-    | 'aof-write'
-    | 'aof-write-active-child'
-    | 'aof-write-alone'
-    | 'aof-write-pending-fsync'
-    | 'command'
-    | 'expire-cycle'
-    | 'eviction-cycle'
-    | 'eviction-del'
-    | 'fast-command'
-    | 'fork'
-    | 'rdb-unlink-temp-file';
-
-
-export function transformArguments (event: EventType): RedisCommandArguments {
+export function transformArguments (event: EventType) {
     return ['LATENCY', 'HISTORY', event];
 }
 
