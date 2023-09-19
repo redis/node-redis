6848f3d207f848330d5da5d491a499365e0a6312
Asher <jangdawit@gmail.com>
2023-09-20 04:59:02 +0900

    Add support for `PUBSUB SHARDNUMSUB` (#2541)
    * Add support for command 'PUBSUB SHARDNUMSUB'
    
    * Use import from PUBSUB_SHARDNUMSUB
    
    * Add test case for non-empty reply
    
    * clean tests
    
    * run tests in redis >= 7, fix integration test
    
    ---------
    
    Co-authored-by: Leibale Eidelman <me@leibale.com>

---
+++ packages/client/lib/commands/PUBSUB_SHARDNUMSUB.ts
@@ -0,0 +1,24 @@
+import { pushVerdictArguments } from './generic-transformers';
+import { RedisCommandArgument, RedisCommandArguments } from '.';
+
+export const IS_READ_ONLY = true;
+
+export function transformArguments(
+    channels?: Array<RedisCommandArgument> | RedisCommandArgument
+): RedisCommandArguments {
+    const args = ['PUBSUB', 'SHARDNUMSUB'];
+
+    if (channels) return pushVerdictArguments(args, channels);
+
+    return args;
+}
+
+export function transformReply(rawReply: Array<string | number>): Record<string, number> {
+    const transformedReply = Object.create(null);
+
+    for (let i = 0; i < rawReply.length; i += 2) {
+        transformedReply[rawReply[i]] = rawReply[i + 1];
+    }
+
+    return transformedReply;
+}
