77308ed670d99fe6440d2f5ab9e7d2ac4c4c7cd0
Leibale <me@leibale.com>
2023-10-03 13:24:00 -0400

    fix for last commit

--- packages/search/lib/commands/CONFIG_GET.ts
+++ packages/search/lib/commands/CONFIG_GET.ts
@@ -16,21 +16,3 @@ export default {
     return transformedReply;
   }
 } as const satisfies Command;
-
-
-export function transformArguments(option: string) {
-    return ['FT.CONFIG', 'GET', option];
-}
-
-interface ConfigGetReply {
-    [option: string]: string | null;
-}
-
-export function transformReply(rawReply: Array<[string, string | null]>): ConfigGetReply {
-    const transformedReply: ConfigGetReply = Object.create(null);
-    for (const [key, value] of rawReply) {
-        transformedReply[key] = value;
-    }
-
-    return transformedReply;
-}
