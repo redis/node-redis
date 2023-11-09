84f484640ffbcb449d936ebbbdcb818de4826eb9
papsavas <papsavas56@gmail.com>
2023-11-09 11:50:11 +0200

    chore(search): export languages

--- packages/search/lib/index.ts
+++ packages/search/lib/index.ts
@@ -1,5 +1,5 @@
 export { default } from './commands';
 
-export { RediSearchSchema, SchemaFieldTypes, SchemaTextFieldPhonetics, SearchReply, VectorAlgorithms } from './commands';
-export { AggregateSteps, AggregateGroupByReducers } from './commands/AGGREGATE';
+export { RediSearchSchema, RedisSearchLanguages, SchemaFieldTypes, SchemaTextFieldPhonetics, SearchReply, VectorAlgorithms } from './commands';
+export { AggregateGroupByReducers, AggregateSteps } from './commands/AGGREGATE';
 export { SearchOptions } from './commands/SEARCH';
