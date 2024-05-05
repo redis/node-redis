91147fd27a942484991b87500fd29cb56f715e6a
Bhavdeep Sethi <bhavdeep@frec.com>
2024-05-04 18:03:05 -0700

    Tests: Update docker image for subpackages

--- packages/time-series/lib/test-utils.ts
+++ packages/time-series/lib/test-utils.ts
@@ -2,14 +2,14 @@ import TestUtils from '@redis/test-utils';
 import TimeSeries from '.';
 
 export default new TestUtils({
-    dockerImageName: 'redislabs/redistimeseries',
+    dockerImageName: 'redis/redis-stack-server',
     dockerImageVersionArgument: 'timeseries-version'
 });
 
 export const GLOBAL = {
     SERVERS: {
         OPEN: {
-            serverArguments: ['--loadmodule /usr/lib/redis/modules/redistimeseries.so'],
+            serverArguments: ['--loadmodule /opt/redis-stack/lib/redistimeseries.so', '--protected-mode no'],
             clientOptions: {
                 modules: {
                     ts: TimeSeries
