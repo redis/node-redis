9252bf271fe88f0e96654dfc1d099e013da565f8
Shaya Potter <shaya@redislabs.com>
2024-06-11 10:38:08 +0300

    add enum for "error" codes for HPEXPIRETIME, but unsure how to expose to user

--- packages/client/lib/commands/HPEXPIRETIME.ts
+++ packages/client/lib/commands/HPEXPIRETIME.ts
@@ -1,6 +1,13 @@
 import { RedisArgument, ArrayReply, NumberReply, Command } from '../RESP/types';
 import { RedisVariadicArgument, pushVariadicArgument } from './generic-transformers';
-   
+
+export const HASH_FIELD_EXPIRETIME = {
+  /** The field does not exist */
+  FIELD_NOT_EXISTS: -2,
+  /** The field has no expiration time */
+  NO_EXPIRATION_TIME: -1,
+} as const;
+
 export default {
   FIRST_KEY_INDEX: 1,
   IS_READ_ONLY: true,
