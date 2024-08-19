70e14b095b8f5f6d5f705f35f76282049744ca28
David W. Dougherty <dwdougherty@gmail.com>
2024-08-19 12:56:09 -0700

    change example name

--- doctests/cmds-string.js
+++ doctests/cmds-string.js
@@ -1,4 +1,4 @@
-// EXAMPLE: set_and_get
+// EXAMPLE: cmds_string
 // REMOVE_START
 import assert from "assert";
 // REMOVE_END
@@ -7,19 +7,21 @@ import assert from "assert";
 import { createClient } from 'redis';
 
 const client = createClient();
-
 client.on('error', err => console.log('Redis Client Error', err));
-
 await client.connect();
-
+await client.del('bike:1')
 // HIDE_END
+
+// STEP_START set_and_get
 await client.set('bike:1', 'Process 134');
 const value = await client.get('bike:1');
 console.log(value);
 // returns 'Process 134'
-//REMOVE_START
+// REMOVE_START
 assert.equal(value, 'Process 134');
-//REMOVE_END
+await client.del('bike:1')
+// REMOVE_END
+// STEP_END
 
 // HIDE_START
 await client.quit();
