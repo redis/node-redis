fb8caf8f5976047b085dc18b09af0f8536504237
borislav ivanov <borislav.ivanov@redis.com>
2025-02-18 14:52:28 +0200

    refactor!: Remove graph module

--- packages/graph/lib/commands/RO_QUERY.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from '@redis/client/dist/lib/RESP/types';
-import QUERY, { parseQueryArguments } from './QUERY';
-
-export default {
-  IS_READ_ONLY: true,
-  parseCommand: parseQueryArguments.bind(undefined, 'GRAPH.RO_QUERY'),
-  transformReply: QUERY.transformReply
-} as const satisfies Command;
