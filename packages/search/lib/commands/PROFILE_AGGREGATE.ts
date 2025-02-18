d647d345261d89637968d832d693aae3ea58059d
borislav ivanov <borislav.ivanov@redis.com>
2025-02-18 11:45:13 +0200

    refactor!(search): simplify PROFILE commands to return raw response
    - BREAKING CHANGE: FT.PROFILE now returns raw response, letting users implement their own parsing

--- packages/search/lib/commands/PROFILE_AGGREGATE.ts
+++ packages/search/lib/commands/PROFILE_AGGREGATE.ts
@@ -1,37 +1,38 @@
 import { CommandParser } from '@redis/client/dist/lib/client/parser';
-import { Command, ReplyUnion } from "@redis/client/dist/lib/RESP/types";
-import AGGREGATE, { AggregateRawReply, FtAggregateOptions, parseAggregateOptions } from "./AGGREGATE";
-import { ProfileOptions, ProfileRawReply, ProfileReply, transformProfile } from "./PROFILE_SEARCH";
-
+import { Command, ReplyUnion, Resp2Reply, UnwrapReply } from '@redis/client/dist/lib/RESP/types';
+import AGGREGATE, { AggregateRawReply, FtAggregateOptions, parseAggregateOptions } from './AGGREGATE';
+import { ProfileOptions, ProfileRawReply, ProfileReply, } from './PROFILE_SEARCH';
+export interface TransformReplyType {
+  2: (reply: UnwrapReply<Resp2Reply<ProfileRawReply<AggregateRawReply>>>) => ProfileReply;
+  3: () => ReplyUnion;
+}
 export default {
   NOT_KEYED_COMMAND: true,
-    IS_READ_ONLY: true,
-    parseCommand(
-      parser: CommandParser,
-      index: string,
-      query: string,
-      options?: ProfileOptions & FtAggregateOptions
-    ) {
-      parser.push('FT.PROFILE', index, 'AGGREGATE');
-    
-      if (options?.LIMITED) {
-        parser.push('LIMITED');
+  IS_READ_ONLY: true,
+  parseCommand(
+    parser: CommandParser,
+    index: string,
+    query: string,
+    options?: ProfileOptions & FtAggregateOptions
+  ) {
+    parser.push('FT.PROFILE', index, 'AGGREGATE');
+
+    if (options?.LIMITED) {
+      parser.push('LIMITED');
+    }
+
+    parser.push('QUERY', query);
+
+    parseAggregateOptions(parser, options)
+  },
+  transformReply: {
+    2: (reply: UnwrapReply<Resp2Reply<ProfileRawReply<AggregateRawReply>>>): ProfileReply => {
+      return {
+        results: AGGREGATE.transformReply[2](reply[0]),
+        profile: reply[1]
       }
-    
-      parser.push('QUERY', query);
-
-      parseAggregateOptions(parser, options)
     },
-    transformReply: {
-      2: (reply: ProfileAggeregateRawReply): ProfileReply => {
-        return {
-          results: AGGREGATE.transformReply[2](reply[0]),
-          profile: transformProfile(reply[1])
-        }
-      },
-      3: undefined as unknown as () => ReplyUnion
-    },
-    unstableResp3: true
-  } as const satisfies Command;
-
-  type ProfileAggeregateRawReply = ProfileRawReply<AggregateRawReply>;
+    3: undefined as unknown as () => ReplyUnion
+  } as TransformReplyType,
+  unstableResp3: true
+} as const satisfies Command;
