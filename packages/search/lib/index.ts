810e8425dd2753d9a8098de407dbfc2a5482d66b
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2025-04-29 15:14:40 +0300

    fix: exports
    align exports with v4 as much as possible

--- packages/search/lib/index.ts
+++ packages/search/lib/index.ts
@@ -1,7 +1,21 @@
-export { default } from './commands';
+export { default } from './commands'
 
-export { SCHEMA_FIELD_TYPE, SchemaFieldType } from './commands/CREATE';
-
-// export { RediSearchSchema, RedisSearchLanguages, SchemaFieldTypes, SchemaTextFieldPhonetics, SearchReply, VectorAlgorithms } from './commands';
-// export { AggregateGroupByReducers, AggregateSteps } from './commands/AGGREGATE';
-// export { SearchOptions } from './commands/SEARCH';
+export { SearchReply } from './commands/SEARCH'
+export { RediSearchSchema } from './commands/CREATE'
+export {
+    REDISEARCH_LANGUAGE,
+    RediSearchLanguage,
+    SCHEMA_FIELD_TYPE,
+    SchemaFieldType,
+    SCHEMA_TEXT_FIELD_PHONETIC,
+    SchemaTextFieldPhonetic,
+    SCHEMA_VECTOR_FIELD_ALGORITHM,
+    SchemaVectorFieldAlgorithm
+} from './commands/CREATE'
+export {
+    FT_AGGREGATE_GROUP_BY_REDUCERS,
+    FtAggregateGroupByReducer,
+    FT_AGGREGATE_STEPS,
+    FtAggregateStep
+} from './commands/AGGREGATE'
+export { FtSearchOptions } from './commands/SEARCH'
