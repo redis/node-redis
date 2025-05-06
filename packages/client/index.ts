d96cb02e85f314ea6b72136e59605f0ff8071ab4
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2025-05-06 14:49:45 +0300

    Tidy up long export line in client index file
    Wrap and sort entries.

--- packages/client/index.ts
+++ packages/client/index.ts
@@ -1,4 +1,12 @@
-export { RedisModules, RedisFunctions, RedisScripts, RespVersions, TypeMapping/*, CommandPolicies*/, RedisArgument } from './lib/RESP/types';
+export {
+  /* CommandPolicies, */
+  RedisArgument,
+  RedisFunctions,
+  RedisModules,
+  RedisScripts,
+  RespVersions,
+  TypeMapping,
+} from './lib/RESP/types';
 export { RESP_TYPES } from './lib/RESP/decoder';
 export { VerbatimString } from './lib/RESP/verbatim-string';
 export { defineScript } from './lib/lua-script';
