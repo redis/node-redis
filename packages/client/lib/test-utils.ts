5c0ae35efe665b82494f845cf28a15ca47add6d7
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2025-06-15 18:50:57 +0300

    chore(tests): bump test container version 8.0.2

--- packages/client/lib/test-utils.ts
+++ packages/client/lib/test-utils.ts
@@ -9,7 +9,7 @@ import RedisBloomModules from '@redis/bloom';
 const utils = TestUtils.createFromConfig({
   dockerImageName: 'redislabs/client-libs-test',
   dockerImageVersionArgument: 'redis-version',
-  defaultDockerVersion: '8.0-M05-pre'
+  defaultDockerVersion: '8.0.2'
 });
 
 export default utils;
