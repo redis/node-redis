d1569aa74dfd3325d24ac232b5fc43902344d530
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2025-06-16 14:53:45 +0300

    properly transform vinfo result

--- packages/client/lib/commands/VINFO.ts
+++ packages/client/lib/commands/VINFO.ts
@@ -1,5 +1,14 @@
 import { CommandParser } from '../client/parser';
-import { RedisArgument, ArrayReply, BlobStringReply, Command } from '../RESP/types';
+import { RedisArgument, Command, UnwrapReply, Resp2Reply, TuplesToMapReply, SimpleStringReply, NumberReply } from '../RESP/types';
+
+export type VInfoReplyMap = TuplesToMapReply<[
+  [SimpleStringReply<'quant-type'>, SimpleStringReply],
+  [SimpleStringReply<'vector-dim'>, NumberReply],
+  [SimpleStringReply<'size'>, NumberReply],
+  [SimpleStringReply<'max-level'>, NumberReply],
+  [SimpleStringReply<'vset-uid'>, NumberReply],
+  [SimpleStringReply<'hnsw-max-node-uid'>, NumberReply],
+]>;
 
 export default {
   IS_READ_ONLY: true,
@@ -14,5 +23,16 @@ export default {
     parser.push('VINFO');
     parser.pushKey(key);
   },
-  transformReply: undefined as unknown as () => ArrayReply<BlobStringReply>
+  transformReply: {
+    2: (reply: UnwrapReply<Resp2Reply<VInfoReplyMap>>): VInfoReplyMap => {
+      const ret = Object.create(null);
+
+      for (let i = 0; i < reply.length; i += 2) {
+        ret[reply[i].toString()] = reply[i + 1];
+      }
+
+      return ret as unknown as VInfoReplyMap;
+    },
+    3: undefined as unknown as () => VInfoReplyMap
+  }
 } as const satisfies Command;
