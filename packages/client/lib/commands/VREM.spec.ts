5a49190f8cdb5d0bbd04f142d80439437f1b7121
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2025-06-16 15:31:43 +0300

    fix vrem return types

--- packages/client/lib/commands/VREM.spec.ts
+++ packages/client/lib/commands/VREM.spec.ts
@@ -27,4 +27,23 @@ describe('VREM', () => {
     client: GLOBAL.SERVERS.OPEN,
     cluster: GLOBAL.CLUSTERS.OPEN
   });
+
+  testUtils.testWithClient('vRem with RESP3', async client => {
+    await client.vAdd('resp3-key', [1.0, 2.0, 3.0], 'resp3-element');
+
+    assert.equal(
+      await client.vRem('resp3-key', 'resp3-element'),
+      true
+    );
+
+    assert.equal(
+      await client.vCard('resp3-key'),
+      0
+    );
+  }, {
+    ...GLOBAL.SERVERS.OPEN,
+    clientOptions: {
+      RESP: 3
+    }
+  });
 });
