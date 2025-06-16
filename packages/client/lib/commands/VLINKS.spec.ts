e6046fc234cd077bfa035278d6a15633a3976ab9
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2025-06-16 15:16:35 +0300

    add resp3 test for vlinks

--- packages/client/lib/commands/VLINKS.spec.ts
+++ packages/client/lib/commands/VLINKS.spec.ts
@@ -17,8 +17,29 @@ describe('VLINKS', () => {
 
     const result = await client.vLinks('key', 'element1');
     assert.ok(Array.isArray(result));
+    assert.equal(result.length, 1)
+    assert.ok(Array.isArray(result[0]));
+    assert.equal(result[0].length, 1);
+    assert.equal(result[0][0], 'element2');
   }, {
     client: GLOBAL.SERVERS.OPEN,
     cluster: GLOBAL.CLUSTERS.OPEN
+  });
+
+  testUtils.testWithClient('vLinks with RESP3', async client => {
+    await client.vAdd('resp3-key', [1.0, 2.0, 3.0], 'element1');
+    await client.vAdd('resp3-key', [1.1, 2.1, 3.1], 'element2');
+
+    const result = await client.vLinks('resp3-key', 'element1');
+    assert.ok(Array.isArray(result));
+    assert.equal(result.length, 1)
+    assert.ok(Array.isArray(result[0]));
+    assert.equal(result[0].length, 1);
+    assert.equal(result[0][0], 'element2');
+  }, {
+    ...GLOBAL.SERVERS.OPEN,
+    clientOptions: {
+      RESP: 3
+    }
   });
 });
