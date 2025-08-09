a5c9c51327d058c6c47afb674fc852e94e79cdda
tomsongrany <tomsongrandy@gmail.com>
2025-08-09 11:40:12 +0000

    Updated latency reset

--- packages/client/lib/commands/LATENCY_RESET.ts
+++ packages/client/lib/commands/LATENCY_RESET.ts
@@ -1,31 +1,24 @@
-import { CommandParser } from '../client/parser'; 
-import { Command } from '../RESP/types'; 
-import { LATENCY_EVENTS, LatencyEvent } from './LATENCY_GRAPH'; 
-
+import { CommandParser } from '../client/parser';
+import { Command } from '../RESP/types';
+import { LATENCY_EVENTS, LatencyEvent } from './LATENCY_GRAPH';
 
 export { LATENCY_EVENTS, LatencyEvent };
 
-// This object defines the LATENCY RESET command's behavior for the Redis client.
 export default {
-
-    NOT_KEYED_COMMAND: true,
-
-
-    IS_READ_ONLY: false,
-
-  
-    parseCommand(parser: CommandParser, ...events: Array<LatencyEvent>) { 
-    
-        const args: Array<string> = ['LATENCY', 'RESET'];
-
-    
-        if (events.length > 0) {
-            args.push(...events);
-        }
-
-   
-        parser.push(...args);
-    },
-
-    transformReply: undefined as unknown as () => number
-} as const satisfies Command; 
+  NOT_KEYED_COMMAND: true,
+  IS_READ_ONLY: false,
+  /**
+   * Constructs the LATENCY RESET command
+   * * @param parser - The command parser
+   * @param events - The latency events to reset. If not specified, all events are reset.
+   * @see https://redis.io/commands/latency-reset/
+   */
+  parseCommand(parser: CommandParser, ...events: Array<LatencyEvent>) {
+    const args = ['LATENCY', 'RESET'];
+    if (events.length > 0) {
+      args.push(...events);
+    }
+    parser.push(...args);
+  },
+  transformReply: undefined as unknown as () => number
+} as const satisfies Command;
