91198483e15a2b70dae392e0e03435dcefe36283
florian-schunk <149071178+florian-schunk@users.noreply.github.com>
2025-08-25 17:28:28 +0100

    un-comment-out test

--- packages/client/lib/client/index.spec.ts
+++ packages/client/lib/client/index.spec.ts
@@ -352,7 +352,6 @@ describe('Client', () => {
       }
     });
 
-    /*
     testUtils.testWithClientSentinel('Timeout with global timeout config (sentinel)', async sentinel => {
       await blockSetImmediate(async () => {
         await assert.rejects(sentinel.HSET('key', 'foo', 'value'), TimeoutError);
@@ -366,7 +365,6 @@ describe('Client', () => {
         }
       }
     });
-    */
 
     testUtils.testWithClient('undefined and null should not break the client', async client => {
       await assert.rejects(
