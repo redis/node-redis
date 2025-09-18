1e18ff633ce34fdf2f79398b19ffc8691683ddbd
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2025-09-18 11:43:38 +0300

    perf(ts)!: move multi() away from RedisClient
    RedisClientMultiCommandType is slow to typecheck because of its recursive
    nature and the fact that it has many ( around 700 ) methods.
    Most of the users dont use multi(), so there is no point to incur the ts
    costs to them. One possible solution is to move the multi() method away from
    the main RedisClient, thus typechecking will not apply to RedisClientMultiCommandType.
    
    This is a breaking change.
    
    Usage before:
    
    import { createClient } from 'redis';
    const client = createClient();
    cont result = await client.multi()
      .set('foo', 3)
      .get('bar')
      .execTyped();
    
    Usage after:
    
    import { createClient, multi } from 'redis';
    const client = createClient();
    cont result = await multi(client)
      .set('foo', 3)
      .get('bar')
      .execTyped();

--- packages/client/index.ts
+++ packages/client/index.ts
@@ -12,7 +12,9 @@ export { VerbatimString } from './lib/RESP/verbatim-string';
 export { defineScript } from './lib/lua-script';
 export * from './lib/errors';
 
+import { RedisFunctions, RedisModules, RedisScripts, RespVersions, TypeMapping } from './lib/RESP/types';
 import RedisClient, { RedisClientOptions, RedisClientType } from './lib/client';
+import RedisClientMultiCommand, { RedisClientMultiCommandType } from './lib/client/multi-command';
 export { RedisClientOptions, RedisClientType };
 export const createClient = RedisClient.create;
 export { CommandParser } from './lib/client/parser';
@@ -37,3 +39,13 @@ export { REDIS_FLUSH_MODES } from './lib/commands/FLUSHALL';
 
 export { BasicClientSideCache, BasicPooledClientSideCache } from './lib/client/cache';
 
+
+
+export const MULTI = < M extends RedisModules, F extends RedisFunctions, S extends RedisScripts, RESP extends RespVersions, TYPE_MAPPING extends TypeMapping >(client: RedisClientType) => {
+  type Multi = new (...args: ConstructorParameters<typeof RedisClientMultiCommand>) => RedisClientMultiCommandType<[], M, F, S, RESP, TYPE_MAPPING>;
+  return new ((this as any).Multi as Multi)(
+    client._executeMulti.bind(client),
+    client._executePipeline.bind(client),
+    client._commandOptions?.typeMapping
+  );
+}
