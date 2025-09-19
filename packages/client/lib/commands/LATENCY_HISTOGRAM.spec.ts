446722944abff81ffec88db3ec39bcaeb465c862
Trofymenko Vladyslav <naughtysora@proton.me>
2025-09-20 01:39:48 +0300

    adding additiona test for command

--- packages/client/lib/commands/LATENCY_HISTOGRAM.spec.ts
+++ packages/client/lib/commands/LATENCY_HISTOGRAM.spec.ts
@@ -20,18 +20,30 @@ describe('LATENCY HISTOGRAM', () => {
     });
   });
 
-  testUtils.testWithClient('client.latencyHistogram', async client => {
-    await client.set('histogram-test-key', 42);
-    const reply = await client.latencyHistogram('set');
-    assert.ok(Array.isArray(reply));
-    const command = reply[0];
-    const histogram = reply[1];
-    assert.strictEqual(typeof command, 'string');
-    assert.ok(Array.isArray(histogram));
-    const [calls, amount, histogram_usec, stats] = histogram;
-    assert.strictEqual(calls, 'calls');
-    assert.strictEqual(typeof amount, 'number');
-    assert.strictEqual(histogram_usec, 'histogram_usec');
-    assert.ok(Array.isArray(stats));
-  }, GLOBAL.SERVERS.OPEN);
+  describe('histogram information', () => {
+    it('unfiltered list', () => {
+      testUtils.testWithClient('client.latencyHistogram', async client => {
+        const reply = await client.latencyHistogram();
+        assert.ok(Array.isArray(reply));
+      }, GLOBAL.SERVERS.OPEN);
+    });
+
+    it('filtered by a command list', () => {
+      testUtils.testWithClient('client.latencyHistogram', async client => {
+        await client.configSet('latency-monitor-threshold', '100');
+        await client.set('histogram-test-key', 42);
+        const reply = await client.latencyHistogram('set');
+        assert.ok(Array.isArray(reply));
+        const command = reply[0];
+        const histogram = reply[1];
+        assert.strictEqual(typeof command, 'string');
+        assert.ok(Array.isArray(histogram));
+        const [calls, amount, histogram_usec, stats] = histogram;
+        assert.strictEqual(calls, 'calls');
+        assert.strictEqual(typeof amount, 'number');
+        assert.strictEqual(histogram_usec, 'histogram_usec');
+        assert.ok(Array.isArray(stats));
+      }, GLOBAL.SERVERS.OPEN);
+    });
+  });
 });
