27c3697c0212ad7b520350b0c6e2b1caebf80f70
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2025-10-28 12:11:02 +0200

    chore(tests): bump test container version 8.4-RC1-pre

--- packages/test-utils/lib/test-utils.ts
+++ packages/test-utils/lib/test-utils.ts
@@ -3,7 +3,7 @@ import TestUtils from './index'
 export const testUtils = TestUtils.createFromConfig({
   dockerImageName: 'redislabs/client-libs-test',
   dockerImageVersionArgument: 'redis-version',
-  defaultDockerVersion: '8.4-M01-pre'
+  defaultDockerVersion: '8.4-RC1-pre'
 });
 
 
