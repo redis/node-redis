f313563ea3cb6c20efd1cead7f1b4b88c78bcf3f
Pavel Pashov <pavel.pashov@redis.com>
2025-11-10 15:15:51 +0200

    fix(otel): optimize metrics tracking by eliminating promise chaining overhead

--- packages/client/lib/client/index.ts
+++ packages/client/lib/client/index.ts
@@ -21,7 +21,7 @@ import { BasicCommandParser, CommandParser } from './parser';
 import SingleEntryCache from '../single-entry-cache';
 import { version } from '../../package.json'
 import EnterpriseMaintenanceManager, { MaintenanceUpdate, MovingEndpointType } from './enterprise-maintenance-manager';
-import { OTelClientAttributes, OTelMetrics } from '../opentelemetry';
+import { OTelClientAttributes } from '../opentelemetry';
 
 export interface RedisClientOptions<
   M extends RedisModules = RedisModules,
@@ -652,7 +652,7 @@ export default class RedisClient<
           .addCommand(cmd, {
             chainId,
             asap
-          })
+          }, this._getClientOTelAttributes())
           .catch(errorHandler)
       );
     }
@@ -1076,17 +1076,12 @@ export default class RedisClient<
     args: ReadonlyArray<RedisArgument>,
     options?: CommandOptions
   ): Promise<T> {
-    const clientAttributes = this._self._getClientOTelAttributes();
-    const recordOperation = OTelMetrics.instance.createRecordOperationDuration(args, clientAttributes);
-
     if (!this._self.#socket.isOpen) {
-      recordOperation(new ClientClosedError());
       return Promise.reject(new ClientClosedError());
     } else if (
       !this._self.#socket.isReady &&
       this._self.#options.disableOfflineQueue
     ) {
-      recordOperation(new ClientOfflineError());
       return Promise.reject(new ClientOfflineError());
     }
 
@@ -1096,29 +1091,11 @@ export default class RedisClient<
       ...options,
     };
 
-    const promise = this._self.#queue.addCommand<T>(args, opts);
+    const promise = this._self.#queue.addCommand<T>(args, opts, this._self._getClientOTelAttributes());
 
-    if (OTelMetrics.isInitialized()) {
-      OTelMetrics.instance.recordPendingRequests(1, clientAttributes);
 
-      const trackedPromise = promise
-        .then((reply) => {
-          recordOperation();
-          OTelMetrics.instance.recordPendingRequests(-1, clientAttributes);
-          return reply;
-        })
-        .catch((err) => {
-          recordOperation(err);
-          OTelMetrics.instance.recordPendingRequests(-1, clientAttributes);
-          throw err;
-        });
-
-      this._self.#scheduleWrite();
-      return trackedPromise;
-    } else {
-      this._self.#scheduleWrite();
-      return promise;
-    }
+    this._self.#scheduleWrite();
+    return promise;
   }
 
   async SELECT(db: number): Promise<void> {
@@ -1354,7 +1331,7 @@ export default class RedisClient<
     const typeMapping = this._commandOptions?.typeMapping;
     const chainId = Symbol('MULTI Chain');
     const promises = [
-      this._self.#queue.addCommand(['MULTI'], { chainId }),
+      this._self.#queue.addCommand(['MULTI'], { chainId }, this._self._getClientOTelAttributes()),
     ];
 
     for (const { args } of commands) {
@@ -1362,12 +1339,12 @@ export default class RedisClient<
         this._self.#queue.addCommand(args, {
           chainId,
           typeMapping
-        })
+        }, this._self._getClientOTelAttributes())
       );
     }
 
     promises.push(
-      this._self.#queue.addCommand(['EXEC'], { chainId })
+      this._self.#queue.addCommand(['EXEC'], { chainId }, this._self._getClientOTelAttributes())
     );
 
     this._self.#scheduleWrite();
@@ -1543,7 +1520,7 @@ export default class RedisClient<
     this._self.#credentialsSubscription = null;
     return this._self.#socket.quit(async () => {
       clearTimeout(this._self.#pingTimer);
-      const quitPromise = this._self.#queue.addCommand<string>(['QUIT']);
+      const quitPromise = this._self.#queue.addCommand<string>(['QUIT'], undefined, this._self._getClientOTelAttributes());
       this._self.#scheduleWrite();
       return quitPromise;
     });
