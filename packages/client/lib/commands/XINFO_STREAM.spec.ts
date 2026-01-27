d8a2f17e671a6a62ec605ff6b01d68b4d8c61b7d
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2026-01-27 19:06:57 +0200

    feat(client): add idempotency fields to XINFO STREAM response (#3162)

--- packages/client/lib/commands/XINFO_STREAM.spec.ts
+++ packages/client/lib/commands/XINFO_STREAM.spec.ts
@@ -29,6 +29,14 @@ describe('XINFO STREAM', () => {
         'entries-added': 0,
         'recorded-first-entry-id': '0-0',
       },
+      ...testUtils.isVersionGreaterThan([8, 6]) && {
+        'idmp-duration': 100,
+        'idmp-maxsize': 100,
+        'pids-tracked': 0,
+        'iids-tracked': 0,
+        'iids-added': 0,
+        'iids-duplicates': 0,
+      },
       groups: 1,
       'first-entry': null,
       'last-entry': null
@@ -36,5 +44,43 @@ describe('XINFO STREAM', () => {
   }, {
     client: GLOBAL.SERVERS.OPEN,
     cluster: GLOBAL.CLUSTERS.OPEN
+  });
+
+  testUtils.testWithClient('xInfoStream with RESP3', async client => {
+    const [, reply] = await Promise.all([
+      client.xGroupCreate('key', 'group', '$', {
+        MKSTREAM: true
+      }),
+      client.xInfoStream('key')
+    ]);
+
+    const expected = Object.assign(Object.create(null), {
+      length: 0,
+      'radix-tree-keys': 0,
+      'radix-tree-nodes': 1,
+      'last-generated-id': '0-0',
+      ...testUtils.isVersionGreaterThan([7, 0]) && {
+        'max-deleted-entry-id': '0-0',
+        'entries-added': 0,
+        'recorded-first-entry-id': '0-0',
+      },
+      ...testUtils.isVersionGreaterThan([8, 6]) && {
+        'idmp-duration': 100,
+        'idmp-maxsize': 100,
+        'pids-tracked': 0,
+        'iids-tracked': 0,
+        'iids-added': 0,
+        'iids-duplicates': 0,
+      },
+      groups: 1,
+      'first-entry': null,
+      'last-entry': null
+    });
+    assert.deepEqual(reply, expected);
+  }, {
+    ...GLOBAL.SERVERS.OPEN,
+    clientOptions: {
+      RESP: 3
+    }
   });
 });
