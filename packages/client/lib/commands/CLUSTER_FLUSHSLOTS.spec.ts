7e12733ac8272b2253d7c93f89e0d34c645f77e6
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2026-04-01 21:45:54 +0300

    Remove CLUSTER_FLUSHSLOTS test (breaks cluster state), uncomment CLUSTER_INFO test

--- packages/client/lib/commands/CLUSTER_FLUSHSLOTS.spec.ts
+++ packages/client/lib/commands/CLUSTER_FLUSHSLOTS.spec.ts
@@ -1,5 +1,4 @@
 import { strict as assert } from 'node:assert';
-import testUtils, { GLOBAL } from '../test-utils';
 import CLUSTER_FLUSHSLOTS from './CLUSTER_FLUSHSLOTS';
 import { parseArgs } from './generic-transformers';
 
@@ -10,13 +9,4 @@ describe('CLUSTER FLUSHSLOTS', () => {
       ['CLUSTER', 'FLUSHSLOTS']
     );
   });
-
-  // TODO: re-enable once cluster CI flakiness is resolved
-  // testUtils.testWithCluster('clusterNode.clusterFlushSlots', async cluster => {
-  //   const client = await cluster.nodeClient(cluster.masters[0]);
-  //   assert.equal(
-  //     await client.clusterFlushSlots(),
-  //     'OK'
-  //   );
-  // }, GLOBAL.CLUSTERS.OPEN);
 });
