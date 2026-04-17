2fdf89e60406390862d538155ce660ce82894b07
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2026-04-17 12:17:49 +0300

    chore(tests): bump default docker test image to 8.8-m02 (#3236)

--- packages/entraid/lib/test-utils.ts
+++ packages/entraid/lib/test-utils.ts
@@ -7,7 +7,7 @@ export const testUtils = TestUtils.createFromConfig({
   dockerImageName: 'redislabs/client-libs-test',
   dockerImageTagArgument: 'redis-tag',
   dockerImageVersionArgument: 'redis-version',
-  defaultDockerVersion: { tag: 'unstable-24425681442-debian', version: '8.8' }
+  defaultDockerVersion: { tag: '8.8-m02', version: '8.8' }
 });
 
 const DEBUG_MODE_ARGS = testUtils.isVersionGreaterThan([7]) ?
--- packages/json/lib/test-utils.ts
+++ packages/json/lib/test-utils.ts
@@ -5,7 +5,7 @@ export default TestUtils.createFromConfig({
   dockerImageName: 'redislabs/client-libs-test',
   dockerImageTagArgument: 'redis-tag',
   dockerImageVersionArgument: 'redis-version',
-  defaultDockerVersion: { tag: 'unstable-24425681442-debian', version: '8.8' }
+  defaultDockerVersion: { tag: '8.8-m02', version: '8.8' }
 });
 
 export const GLOBAL = {
