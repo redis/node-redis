4067763df9b49405997650457fa323963f888d4c
vlady-kotsev <vladygk8@gmail.com>
2026-09-15 17:36:17 +0300

    feat(bloom): add cuckoo mexists command

---
+++ packages/bloom/lib/commands/cuckoo/MEXISTS.ts
@@ -0,0 +1,13 @@
+import { CommandParser } from '@redis/client/dist/lib/client/parser';
+import { RedisArgument, Command } from '@redis/client/dist/lib/RESP/types';
+import { RedisVariadicArgument } from '@redis/client/dist/lib/commands/generic-transformers';
+import { transformBooleanArrayReply } from '@redis/client/dist/lib/commands/generic-transformers';
+
+export default {
+  parseCommand(parser: CommandParser, key: RedisArgument, items: RedisVariadicArgument) {
+    parser.push('CF.MEXISTS');
+    parser.pushKey(key);
+    parser.pushVariadic(items);
+  },
+  transformReply: transformBooleanArrayReply
+} as const satisfies Command;
